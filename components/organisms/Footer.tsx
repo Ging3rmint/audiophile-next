05fb68590a98ae77789a5803fbcc0f75b16e3d56
Nate <68282945+Ging3rmint@users.noreply.github.com>
2022-04-23 01:39:10 +0800

    Update Footer.tsx

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -165,10 +165,10 @@ const Footer: React.FC<PropTypes> = ({ pathName }) => {
         </div>
         <div className='middle'>
           <p>
-            Audiophile is an all in one stop to fulfill your audio needs. We're
-            a small team of music lovers and sound specialists who are devoted
-            to helping you get the most out of personal audio. Come and visit
-            our demo facility - we&apos;re open 7 days a week.
+            Audiophile is an all in one stop to fulfill your audio needs.
+            We&apos;re a small team of music lovers and sound specialists who
+            are devoted to helping you get the most out of personal audio. Come
+            and visit our demo facility - we&apos;re open 7 days a week.
           </p>
           <div className='middle__social-media'>
             <Link href='/'>
